6581311385f4461dcd2b9c40fd8251d5303465b2
JavohirO2 <97465850+JavohirO2@users.noreply.github.com>
2024-02-28 09:22:50 -0500

    Add files via upload

---
+++ README.txt.txt
@@ -0,0 +1,4 @@
+The program is basic qr code generator using open source code from this github https://github.com/davidshimjs/qrcodejs
+The program can create qr code with text or url
+The tracking can be done through the url
+Still working on adding a emblem in the middle of the qr code
---
+++ script.js
@@ -0,0 +1,25 @@
+function generateQRCode() {
+    var inputValue = document.getElementById('text-input').value;
+	
+	
+	
+    
+    // Ensure there is a value to generate QR code
+    if (inputValue.trim() === '') {
+        alert('Please enter text or URL to generate QR code.');
+        return;
+    }
+
+    // Clear existing QR code
+    document.getElementById('qrcode').innerHTML = '';
+	
+
+    // Create QR code
+    var qrcode = new QRCode(document.getElementById('qrcode'), {
+        text: inputValue,
+        width: 128,
+        height: 128
+    });
+	
+	
+}
